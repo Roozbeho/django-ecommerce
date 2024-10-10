ca3643ca5b407d1b69bdb677a4c17a572f90af87
Roozbeho <roozbehtavanaee@gmail.com>
2024-10-10 22:02:40 +0330

    customize admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,18 +1,50 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import urlencode
+from django.utils.safestring import mark_safe
 
 from .models import Address, Customer, OtpCode
 
 
-class AddressAdmin(admin.StackedInline):
-    model = Address
+# class AddressAdmin(admin.StackedInline):
+    # model = Address
 
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['get_fullname', 'customer__email', 'phone_number', 'state', 'city', 'is_default']
+    list_filter = ['state', 'city']
+    search_fields = ['customer', 'first_name__istartswith', 'last_name__istartswith']
+    list_select_related = ['customer']
+    list_per_page = 25
 
+    def get_fullname(self, address):
+        return address.first_name + ' ' + address.last_name
+    
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["email", "username", "is_verified"]
-    inlines = [AddressAdmin]
+    list_display = ["email", "username", "is_verified", 'address', 'order']
+    list_filter = ['is_superuser', 'is_verified', 'is_active']
+    search_fields = ['username__istartswith', 'email__istartswith']
+    list_per_page = 25
 
 
+
+    @admin.display(description='Addresses')
+    def address(self, customer):
+        url = (
+            reverse('admin:account_address_changelist')
+            + '?' + urlencode({'customer__id':str(customer.id)})
+        )
+        return mark_safe(f"<a href='{url}'>User Addresses</a>")
+    
+    @admin.display(description='Orders')
+    def order(self, customer):
+        url = (
+            reverse('admin:order_order_changelist')
+            + '?' + urlencode({'customer__id':str(customer.id)})
+        )
+        return mark_safe(f"<a href='{url}'>User Orders</a>")
+    
 @admin.register(OtpCode)
 class OtpCodeAdmin(admin.ModelAdmin):
     readonly_fields = ["otp_code"]
--- shop/admin.py
+++ shop/admin.py
@@ -1,24 +1,90 @@
 from django.contrib import admin
 
-from .models import (Category, Product, ProductImage, ProductSpecification,
-                     ProductSpecificationValue, Review, Sub_Category)
-
-
-class SubCategoryinline(admin.TabularInline):
-    model = Sub_Category
-    prepopulated_fields = {"slug": ["name"]}
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    Review,
+    Sub_Category,
+)
+from django.urls import reverse
+from django.utils.html import urlencode
+from django.utils.safestring import mark_safe
+from django.db.models import Count
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "slug", "is_active"]
+    list_display = ["name", "slug", "is_active", "sub_category"]
     list_filter = ["name", "is_active", "created_at"]
+    search_fields = ["name__istartswith"]
     prepopulated_fields = {"slug": ["name"]}
+    list_per_page = 25
+    admin_priority = 1
 
-    inlines = [SubCategoryinline]
+    @admin.display(description="sub categories")
+    def sub_category(self, category):
+        url = (
+            reverse("admin:shop_sub_category_changelist")
+            + "?"
+            + urlencode({"category__id": str(category.id)})
+        )
+        return mark_safe(
+            f'<a href="{url}">{category.subcategory_count} related sub categories</a>'
+        )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(subcategory_count=Count("subcategories"))
+        )
 
 
-admin.site.register(ProductSpecification)
+@admin.register(Sub_Category)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_active", "category__name", "product", "specification"]
+    search_fields = ["name__istartswith", "category__name"]
+    list_filter = ["is_active", "category__name"]
+    list_select_related = ["category"]
+    list_per_page = 25
+    admin_priority = 2
+
+    @admin.display(description="Products")
+    def product(self, sub_category):
+        url = (
+            reverse("admin:shop_product_changelist")
+            + "?"
+            + urlencode({"sub_category__id": str(sub_category.id)})
+        )
+
+        return mark_safe(
+            f'<a href="{url}">{sub_category.product_count} related products</a>'
+        )
+
+    @admin.display(description="Specifications")
+    def specification(self, sub_category):
+        url = (
+            reverse("admin:shop_productspecification_changelist")
+            + "?"
+            + urlencode({"sub_category__id": str(sub_category.id)})
+        )
+        return mark_safe(f'<a href="{url}">related specifications</a>')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(product_count=Count("products"))
+
+
+@admin.register(ProductSpecification)
+class productspecificationAdmin(admin.ModelAdmin):
+    list_display = ["name", "sub_category__name"]
+    list_filter = ["name", "sub_category__name"]
+    search_fields = ["name"]
+    list_select_related = ["sub_category"]
+    list_per_page = 25
+    admin_priority = 3
 
 
 class ProductSpecificationValueInline(admin.TabularInline):
@@ -38,17 +104,44 @@ class ProductAdmin(admin.ModelAdmin):
         "is_active",
         "viewed",
         "created_at",
+        "review",
     ]
     list_filter = ["sub_category", "name", "price", "created_at"]
     prepopulated_fields = {"slug": ["name"]}
+    search_fields = ["name__istartswith"]
     inlines = [ProductSpecificationValueInline, ProductImageInline]
+    list_per_page = 25
+    admin_priority = 4
+
+    @admin.display(description="Products")
+    def review(self, product):
+        url = (
+            reverse("admin:shop_review_changelist")
+            + "?"
+            + urlencode({"product__id": str(product.id)})
+        )
+        return mark_safe(
+            f'<a href="{url}">{product.reviews_count} reviews on thise product</a>'
+        )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(reviews_count=Count("reviews"))
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ["customer", "product", "title", "is_buyer", "is_active", "created_at"]
+    list_display = [
+        "customer",
+        "product",
+        "title",
+        "is_buyer",
+        "is_active",
+        "created_at",
+    ]
     list_filter = ["product", "is_active", "created_at"]
-    search_fields = ["customer", "product"]
+    search_fields = ["customer"]
+    list_per_page = 25
+    admin_priority = 5
 
 
 # @admin.register(Rating)
